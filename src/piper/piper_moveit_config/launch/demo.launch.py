fadd1360d79381abfec2ef4bb0745aed6cf1eea5
HoranTian <1915168688@qq.com~>
2025-02-10 17:47:29 +0800

    删除joint_states正序

--- src/piper/piper_moveit_config/launch/demo.launch.py
+++ src/piper/piper_moveit_config/launch/demo.launch.py
@@ -5,19 +5,9 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 
 def generate_launch_description():
+    # 设置 MoveIt 配置
     moveit_config = MoveItConfigsBuilder("piper", package_name="piper_moveit_config").to_moveit_configs()
-    
-    # 获取当前目录
-    current_directory = os.path.dirname(os.path.realpath(__file__))
-    
-    # 添加执行 ordered_joint_states.py 脚本的动作
-    ordered_joint_states_script = ExecuteProcess(
-        cmd=["python3", "ordered_joint_states.py"],
-        cwd=current_directory,  # 设置为当前目录
-        output="screen"
-    )
-    
+      
     return LaunchDescription([
-        generate_demo_launch(moveit_config),
-        ordered_joint_states_script
+        generate_demo_launch(moveit_config),  # 加载 MoveIt 配置
     ])
